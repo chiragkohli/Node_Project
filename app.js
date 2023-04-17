3d5ed5e18334e0b6ebf9ff96c933a91c2fe317cc
Chirag Kohli <chirag.kohli@nagarro.com>
2023-04-17 14:44:44 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const HOST = 'localhost';
+const PORT = 8000;
+
+const app = express();
+
+app.use('/',(req, res, next) => {
+    res.send(`<html><body><h1>Hello! from Node JS</h1></body></html>`);
+});
+
+app.listen(PORT, HOST, () => {
+    console.log(`Server is running on http://${HOST}:${PORT}`);
+});
